9b977a8c910fce780693e2f125a0eeb98f080139
Jaime Fernández <jaimefdez7@gmail.com>
2024-10-24 18:07:24 +0200

    add test nvim extension

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -450,7 +450,7 @@ require('lazy').setup({
             python = {
               analysis = {
                 -- Ignore all files for analysis to exclusively use Ruff for linting
-                ignore = { '*' },
+                -- ignore = { '*' },
               },
             },
           },
