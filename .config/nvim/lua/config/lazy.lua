68c2250e8078b32e84779087d16d53c82fc1b9f1
Jaime Fernández <jaimefdez7@gmail.com>
2025-02-19 12:53:01 +0100

    fix: remove redundant leader mapping in init.lua for nvim

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -15,9 +15,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-vim.g.mapleader = ' '
-vim.g.maplocalleader = '\\'
-
 local spec = {
   { import = 'plugins' },
 }
